6bf106a4993ca65080982c3c5805f25642aada12
Nelson Arevalo <nelson.arevalo2119@gmail.com>
2022-10-30 18:17:42 -0500

    Adding swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,38 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * This function reformat the response of the api.
+     * @param bool $status - status of request.
+     * @param array $message - message of the request.
+     * @param mixed $data - data obtained on the request.
+     * @return JsonResponse
+     */
+    public function responseApi(bool $status, array $message, mixed $data): JsonResponse
+    {
+        $type = $message["type"];
+
+        if($type === "success") {
+            $message["code"] = 200;
+        } else if ($type === "error") {
+            $message["code"] = 500;
+        } else if ($type === "warning") {
+            $message["code"] = 300;
+        } else {
+            abort(500);
+        }
+
+        return response()->json(array(
+           "transaction" => array("status" => $status),
+           "message" => $message,
+           "data" => $data
+        ), $message["code"]);
+    }
 }
